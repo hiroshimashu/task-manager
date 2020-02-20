2c6f11ebc6bd24a1b994ec55f6d153d039128758
hiroshimashu <hiroshimashu@gmail.com>
2020-02-20 21:33:11 +0900

    Add: Implement createTasks method on task_service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from "./tasks.service";
 import { Task } from "./task.model";
 
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private taskService: TasksService) { }
+    constructor(private tasksService: TasksService) { }
     
     @Get()
     getAllTasks(): Task[] {
-        return this.taskService.getAllTasks();
+        return this.tasksService.getAllTasks();
+    } 
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string
+    ) {
+        return this.tasksService.createTask(title, description);
     }
  }
